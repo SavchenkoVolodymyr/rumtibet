ae39cde74fa5110de914ed7295a6edc0733144eb
SavchenkoVolodymyr <v1987savchenko@ukr.net>
2025-02-10 22:35:36 +0200

    feat js for hero form

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,28 @@
 const body = document.querySelector('body');
-document.querySelector('.dropdown__button').addEventListener('click', function () {
-    document.querySelector('.dropdown__list').classList.add('dropdown__list--visible');
+
+document.querySelectorAll('.dropdown').forEach(function (dropForAll) {
+
+	const dropdownButton = dropForAll.querySelector('.dropdown__button');
+	const dropdownList = dropForAll.querySelector('.dropdown__list');
+	const dropdownListAitems = dropdownList.querySelectorAll('.dropdown__item');
+	const dropdownInput = dropForAll.querySelector('.dropdown__input_hidden');
+
+	dropdownButton.addEventListener('click', function () {
+		dropdownList.classList.toggle('dropdown__list--visible');
+	});
+
+	dropdownListAitems.forEach(function (listItem) {
+		listItem.addEventListener('click', function (e) {
+			e.stopPropagation();
+			dropdownButton.innerText = this.innerText;
+			dropdownInput.value = this.dataset.value;
+			dropdownList.classList.remove('dropdown__list--visible');
+		});
+	});
+
+	document.addEventListener('click', function (e) {
+		if (e.target !== dropdownButton) {
+			dropdownList.classList.remove('dropdown__list--visible');
+		}
+	});
 });
