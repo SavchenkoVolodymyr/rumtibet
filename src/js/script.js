74f343f533c60f223a75db595a1baa26c5b91415
SavchenkoVolodymyr <v1987savchenko@ukr.net>
2025-02-10 21:16:43 +0200

    start fix hero form

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,4 @@
 const body = document.querySelector('body');
+document.querySelector('.dropdown__button').addEventListener('click', function () {
+    document.querySelector('.dropdown__list').classList.add('dropdown__list--visible');
+});
